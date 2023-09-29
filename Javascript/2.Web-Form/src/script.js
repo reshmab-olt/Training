64036b664d02205dc7f499663ec6d606a8dc8db7
Reshma B <139335112+reshmab-olt@users.noreply.github.com>
2023-09-29 08:58:03 +0530

    Update script.js

--- Javascript/2.Web-Form/src/script.js
+++ Javascript/2.Web-Form/src/script.js
@@ -196,7 +196,6 @@ function isAllowed(element, expression, index, message) {
 
   if (!element.value.match(expression)) {
     document.querySelectorAll('.error')[index].textContent = message;
-    isValid = false;
   }
 }
 
