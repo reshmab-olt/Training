0e8bf9099cac1605e8253822508bf526b0bc9a2d
Reshma B <139335112+reshmab-olt@users.noreply.github.com>
2023-09-29 17:19:11 +0530

    Update script.js

--- Javascript/2.Web-Form/src/script.js
+++ Javascript/2.Web-Form/src/script.js
@@ -82,7 +82,8 @@ function isValidDate(dateString) {
     isNaN(day) ||
     year.length !== 4 ||
     month < 1 || month > 12 ||
-    day < 1 || day > 31
+    day < 1 || day > 31 ||
+    (month === '02' && day > 27)
   ) {
     return false;
   }
