1d4036a4dfd9fbc1d777a9fa02ce9ee5da1ec22f
Reshma B <139335112+reshmab-olt@users.noreply.github.com>
2023-10-04 13:36:59 +0530

    update calculator

--- jQuery/1.Basic-Calculator/src/script.js
+++ jQuery/1.Basic-Calculator/src/script.js
@@ -1,38 +1,38 @@
+let display = $("#display");
+const clearButton = $("#clear");
+const equalButton = $("#equal");
 
-    let display = $("#display");
-    const clearButton = $("#clear");
-    const equalButton = $("#equal");
-
-    function calculateResult(){
-
-        try {
-            let result = math.evaluate(display.val());
-            if (!isNaN(result) && result % 1 !== 0) {
-                result = result.toFixed(14);
-                result = result.replace(/(\.[0-9]*[1-9])0+$/, "$1");
-            }
-            display.val(result.toString());
-        } catch (error) {
-            display.val("Error");
+//Calculation
+function calculateResult() {
+    try {
+        let result = math.evaluate(display.val());
+        if (!isNaN(result) && result % 1 !== 0) {
+            result = result.toFixed(14);
+            result = result.replace(/(\.[0-9]*[1-9])0+$/, "$1");
         }
+        display.val(result.toString());
+    } catch (error) {
+        display.val("Error");
     }
+}
 
-    $(".input").click(function() {
+$(".input").click(function () {
+    let currentVal = display.val();
+    let buttonVal = $(this).text();
+    
+    if (currentVal === "Error") {
+        display.val(buttonVal); 
+    } else if (buttonVal === "." && currentVal.endsWith(".")) {
+        return;
+    } else {
+        display.val(currentVal + buttonVal);
+    }
+});
 
-        let currentVal = display.val();
-        let buttonVal = $(this).text();
+equalButton.click(function () {
+    calculateResult();
+});
 
-        if (buttonVal === "." && currentVal.endsWith(".")) {
-            return;
-        } else {
-            display.val(currentVal + buttonVal);
-        }
-    });
-
-    equalButton.click(function() {
-        calculateResult();
-     });
-     
-     clearButton.click(function() {
-        display.val("");
-    });
+clearButton.click(function () {
+    display.val("");
+});
