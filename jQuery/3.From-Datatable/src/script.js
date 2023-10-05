220394e81fb09a30e91c543e4f76cb55e35dece7
Reshma B <139335112+reshmab-olt@users.noreply.github.com>
2023-10-05 13:05:19 +0530

    update tabledata

--- jQuery/3.From-Datatable/src/script.js
+++ jQuery/3.From-Datatable/src/script.js
@@ -1,4 +1,52 @@
+$(document).ready(function () {
+    var dataTable = $('table').DataTable({
+        columns: [
+            { data: 'place_of_publication' },
+            { data: 'start_year' },
+            { data: 'publisher' },
+            { data: 'county' },
+            { data: 'edition' },
+            { data: 'frequency' },
+            { data: 'url' },
+            { data: 'id' },
+            { data: 'subject' },
+            { data: 'city' },
+            { data: 'language' },
+            { data: 'title' },
+            { data: 'holding_type' },
+            { data: 'end_year' },
+            { data: 'alt_title' },
+            { data: 'note' },
+            { data: 'lccn' },
+            { data: 'state' },
+            { data: 'place' },
+            { data: 'country' },
+            { data: 'type' },
+            { data: 'title_normal' },
+            { data: 'oclc' },
+        ]
+    });
 
-$(document).ready(function() {
-    $('table').DataTable();
+    $('#searchButton').on('click', function () {
+        var searchId = $('#searchId').val();
+
+        dataTable.clear().draw();
+
+        $.ajax({
+            url: "https://chroniclingamerica.loc.gov/search/titles/results/?terms=oakland&format=json&page=5",
+            type: "GET",
+            dataType: "json",
+            success: function (data) {
+                var filteredData = data.items.filter(function (item) {
+                    return (searchId === "" || item.id.includes(searchId) || item.type.includes(searchId));
+                       
+                });
+
+                dataTable.rows.add(filteredData).draw();
+            },
+            error: function (error) {
+                console.log('Error:', error);
+            }
+        });
+    });
 });
