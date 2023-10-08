b4f0ee10036c0210b6d41f08bf2eaaca0aa9d492
reshmab-olt <reshma.b@optiologic.com>
2023-10-08 21:52:35 +0530

    update datatable

--- jQuery/3.jQuery-Datatable/src/script.js
+++ jQuery/3.jQuery-Datatable/src/script.js
@@ -1,94 +1,116 @@
+let dataTable;
 
-    let dataTable = $('#dataTable').DataTable({
-        "paging": true, 
-        "pageLength": 10,
-        "dom": 'lrtip',
-        "pagingType": "full_numbers" 
+dataTable = $('#dataTable').DataTable({
+    "paging": true,
+    "pageLength": 10,
+    "dom": 'lrtip',
+    "pagingType": "full_numbers"
+});
+
+let currentPage = 1;
+let itemsPerPage = 10;
+let totalItems = 0;
+
+function updateTable(page) {
+    let startIndex = (page - 1) * itemsPerPage;
+    let endIndex = startIndex + itemsPerPage;
+
+    dataTable.page(startIndex / itemsPerPage).draw(false);
+    $('#currentPage').text(page);
+    currentPage = page;
+
+    let totalPage = Math.ceil(dataTable.rows().count() / itemsPerPage);
+
+    $('#prevPage').prop('disabled', currentPage === 1);
+    $('#nextPage').prop('disabled', currentPage === totalPage);
+    updateSerialNumbers();
+}
+
+$('#nextPage').click(function () {
+    let totalPage = Math.ceil(dataTable.rows().count() / itemsPerPage);
+
+    if (currentPage < totalPage) {
+        currentPage++;
+        updateTable(currentPage);
+    }
+});
+$('.page-button').click(function () {
+    let page = parseInt($(this).text());
+
+    updateTable(page);
+});
+
+$('#prevPage').click(function () {
+    if (currentPage > 1) {
+        currentPage--;
+        updateTable(currentPage);
+    }
+});
+
+function updateSerialNumbers() {
+    dataTable.rows().every(function (rowIdx, tableLoop, rowLoop) {
+        let data = this.data();
+
+        data[0] = rowIdx + 1;
+        this.data(data);
     });
+}
 
-    let currentPage = 1;
-    let itemsPerPage = 10;
-    let totalItems = 0;
+$('#searchForm').submit(function (e) {
+    e.preventDefault();
 
-    
-    function updateTable(page) {
-        let startIndex = (page - 1) * itemsPerPage;
-        let endIndex = startIndex + itemsPerPage;
-        dataTable.page(startIndex / itemsPerPage).draw(false);
-        $('#currentPage').text(page);
-        currentPage = page;
+    let searchValueLccn = $('#searchInputLccn').val();
+    let searchValueFrequency = $('#searchInputFrequency').val();
+    let searchUrl = 'https://chroniclingamerica.loc.gov/search/titles/results/?format=json';
 
-        let totalPage = Math.ceil(dataTable.rows().count() / itemsPerPage);
-        $('#prevPage').prop('disabled', currentPage === 1);
-        $('#nextPage').prop('disabled', currentPage === totalPage);
+    if (!searchValueLccn && !searchValueFrequency) {
+        $('#paginationContainer').hide();
+        dataTable.clear().draw();
+        return;
     }
 
-    $('#nextPage').click(function () {
-        let totalPage = Math.ceil(dataTable.rows().count() / itemsPerPage);
-        if (currentPage < totalPage) {
-            currentPage++;
-            updateTable(currentPage);
+    if (searchValueLccn) {
+        searchUrl += '&lccn=' + encodeURIComponent(searchValueLccn);
+    }
+
+    if (searchValueFrequency) {
+        searchUrl += '&frequency=' + encodeURIComponent(searchValueFrequency);
+    }
+
+    $('#paginationContainer').show();
+
+    $.ajax({
+        url: searchUrl,
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            dataTable.clear().draw();
+
+            $.each(data.items, function (index, item) {
+                dataTable.row.add([
+                    index + 1,
+                    item.place_of_publication,
+                    item.start_year,
+                    item.publisher,
+                    item.frequency,
+                    item.url,
+                    item.id,
+                    item.subject,
+                    item.city,
+                    item.language,
+                    item.title,
+                    item.lccn,
+                    item.state,
+                    item.country,
+                    item.oclc
+                ]);
+            });
+
+            dataTable.draw();
+            updateSerialNumbers();
+        },
+        error: function () {
+            console.error('Error fetching data.');
         }
     });
-    $('.page-button').click(function () {
-        let page = parseInt($(this).text());
-        updateTable(page);
-    });
-
-    $('#prevPage').click(function () {
-        if (currentPage > 1) {
-            currentPage--;
-            updateTable(currentPage);
-        }
-    });
-
-    $('.page-button').click(function () {
-        let page = parseInt($(this).text());
-        updateTable(page);
-    });
-    $('#searchForm').submit(function (e) {
-        e.preventDefault();
-        let searchValue = $('#searchInput').val();
-        $('#paginationContainer').show();
-        $.ajax({
-            url: 'https://chroniclingamerica.loc.gov/search/titles/results/?terms=oakland&format=json&lccn=' + searchValue,
-            type: 'GET',
-            dataType: 'json',
-            success: function (data) {
-                dataTable.clear();
-                $.each(data.items, function (index, item) {
-                    dataTable.row.add([
-                        item.place_of_publication,
-                        item.start_year,
-                        item.publisher,
-                        item.county,
-                        item.edition,
-                        item.frequency,
-                        item.url,
-                        item.id,
-                        item.subject,
-                        item.city,
-                        item.language,
-                        item.title,
-                        item.holding_type,
-                        item.end_year,
-                        item.alt_title,
-                        item.note,
-                        item.lccn,
-                        item.state,
-                        item.place,
-                        item.country,
-                        item.type,
-                        item.title_normal,
-                        item.oclc
-                    ]);
-                });
-
-                dataTable.draw();
-            },
-            error: function () {
-                console.error('Error fetching data.');
-            }
-        });
-    });
-
+});
