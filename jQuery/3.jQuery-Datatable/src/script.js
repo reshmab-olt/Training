0fab32547db38975b05c2400cac54123e6fe16b1
reshmab-olt <reshma.b@optiologic.com>
2023-10-09 22:40:03 +0530

    update datatable

--- jQuery/3.jQuery-Datatable/src/script.js
+++ jQuery/3.jQuery-Datatable/src/script.js
@@ -1,117 +1,167 @@
-const dataTable = $('#dataTable').DataTable();
-
+const itemsPerPage = 7;
 let currentPage = 1;
-let itemsPerPage = 10;
+
+const dataTable = $('#dataTable').DataTable({
+    paging: true,
+    pageLength: itemsPerPage,
+});
+
+function updateDataTable() {
+    dataTable.page(currentPage - 1).draw(false);
+}
+
+function getTotalPages() {
+    return Math.ceil(dataTable.rows().count() / itemsPerPage);
+}
 
 function updateSerialNumbers() {
-  dataTable.rows().every(function serialNumber(rowIdx) {
-    const data = this.data();
+    dataTable.rows().every(function serialNumber(rowIdx) {
+        const data = this.data();
 
-    data[0] = rowIdx + 1;
-    this.data(data);
-    return 0;
-  });
+        data[0] = rowIdx + 1;
+        this.data(data);
+        return 0;
+    });
 }
 
-function updateTable(page) {
-  const startIndex = (page - 1) * itemsPerPage;
+function updatePaginationControls() {
+    const totalPages = getTotalPages();
 
-  dataTable.page(startIndex / itemsPerPage).draw(false);
-  $('#currentPage').text(page);
-  currentPage = page;
+    const paginationContainer = $('#paginationContainer');
 
-  const totalPage = Math.ceil(dataTable.rows().count() / itemsPerPage);
+    paginationContainer.empty();
 
-  if (totalPage <= 1 || dataTable.rows().count() <= itemsPerPage) {
-    $('#paginationContainer').hide();
-  } else {
-    $('#paginationContainer').show();
-  }
+    const prevButton = $('<button>')
+        .addClass('page-button')
+        .text('Previous')
+        .click(function () {
+            if (currentPage > 1) {
+                currentPage--;
+                updateDataTable();
+                updatePaginationControls();
+            }
+        });
 
-  $('#prevPage').prop('disabled', currentPage === 1);
-  $('#nextPage').prop('disabled', currentPage === totalPage);
-  updateSerialNumbers();
+        if (currentPage === 1) {
+            prevButton.prop('disabled', true);
+        }  
+
+    paginationContainer.append(prevButton);
+
+    for (let i = 1; i <= totalPages; i++) {
+        const pageButton = $('<button>')
+            .addClass('page-button')
+            .text(i)
+            .click(function () {
+                currentPage = i;
+                updateDataTable();
+                updatePaginationControls();
+            });
+
+        if (i === currentPage) {
+            pageButton.addClass('active');
+        }
+
+        paginationContainer.append(pageButton);
+    }
+
+    const nextButton = $('<button>')
+        .addClass('page-button')
+        .text('Next')
+        .click(function () {
+            if (currentPage < totalPages) {
+                currentPage++;
+                updateDataTable();
+                updatePaginationControls();
+            }
+        });
+        if (currentPage === totalPages) {
+            nextButton.prop('disabled', true);
+        }
+
+    paginationContainer.append(nextButton);
 }
 
-$('#nextPage').click(() => {
-  const totalPage = Math.ceil(dataTable.rows().count() / itemsPerPage);
-
-  if (currentPage < totalPage) {
-    currentPage++;
-    updateTable(currentPage);
-  }
-});
-$('.page-button').click(function () {
-  const page = parseInt($(this).text());
-
-  updateTable(page);
+$('#prevPage').click(function (e) {
+    e.preventDefault();
+    if (currentPage > 1) {
+        currentPage--;
+        updateDataTable();
+        updatePaginationControls();
+    }
 });
 
-$('#prevPage').click(() => {
-  if (currentPage > 1) {
-    currentPage--;
-    updateTable(currentPage);
-  }
+$('#nextPage').click(function (e) {
+    e.preventDefault();
+    if (currentPage < getTotalPages()) {
+        currentPage++;
+        updateDataTable();
+        updatePaginationControls();
+    }
 });
 
 $('#searchForm').submit((e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  const searchValueLccn = $('#searchInputLccn').val();
-  const searchValueFrequency = $('#searchInputFrequency').val();
-  const searchValueTerm = $('#terms').val();
-  let searchUrl = 'https://chroniclingamerica.loc.gov/search/titles/results/?format=json';
+    const searchValueLccn = $('#searchInputLccn').val();
+    const searchValueFrequency = $('#searchInputFrequency').val();
+    const searchValueTerm = $('#terms').val();
+    let searchUrl = 'https://chroniclingamerica.loc.gov/search/titles/results/?format=json';
 
-  if (searchValueLccn) {
-    searchUrl += `&lccn=${encodeURIComponent(searchValueLccn)}`;
-  }
+    if (searchValueLccn) {
+        searchUrl += `&lccn=${encodeURIComponent(searchValueLccn)}`;
+    }
 
-  if (searchValueFrequency) {
-    searchUrl += `&frequency=${encodeURIComponent(searchValueFrequency)}`;
-  }
+    if (searchValueFrequency) {
+        searchUrl += `&frequency=${encodeURIComponent(searchValueFrequency)}`;
+    }
 
-  if (searchValueTerm) {
-    searchUrl += `&terms=${encodeURIComponent(searchValueTerm)}`;
-  }
+    if (searchValueTerm) {
+        searchUrl += `&terms=${encodeURIComponent(searchValueTerm)}`;
+    }
 
-  itemsPerPage = 10;
+    $('#paginationContainer').show();
 
-  $('#paginationContainer').show();
+    $.ajax({
+        url: searchUrl,
+        type: 'GET',
+        dataType: 'json',
+        success(data) {
+            dataTable.clear().draw();
 
-  $.ajax({
-    url: searchUrl,
-    type: 'GET',
-    dataType: 'json',
-    success(data) {
-      dataTable.clear().draw();
+            $.each(data.items, (index, item) => {
+                dataTable.row.add([
+                    index + 1,
+                    item.place_of_publication,
+                    item.start_year,
+                    item.publisher,
+                    item.frequency,
+                    item.city,
+                    item.language,
+                    item.title,
+                    item.lccn,
+                    item.state,
+                ]);
+            });
 
-      $.each(data.items, (index, item) => {
-        dataTable.row.add([
-          index + 1,
-          item.place_of_publication,
-          item.start_year,
-          item.publisher,
-          item.frequency,
-          item.city,
-          item.language,
-          item.title,
-          item.lccn,
-          item.state,
-        ]);
-      });
+            dataTable.draw();
+            updateSerialNumbers();
 
-      dataTable.draw();
-      updateSerialNumbers();
-      $('#searchInputLccn').val('');
-      $('#searchInputFrequency').val('');
-      $('#terms').val('');
+            const totalPages = getTotalPages();
 
-      if (dataTable.rows().count() <= itemsPerPage) {
-        $('#paginationContainer').hide();
-      }
-    },
-    error() {
-      console.error('Error fetching data.');
-    },
-  });
+            if (totalPages <= 1) {
+                $('#paginationContainer').hide();
+            } else {
+                $('#paginationContainer').show();
+                updatePaginationControls();
+            }
+
+            $('#searchInputLccn').val('');
+            $('#searchInputFrequency').val('');
+            $('#terms').val('');
+        },
+        error() {
+            console.error('Error fetching data.');
+        },
+    });
 });
